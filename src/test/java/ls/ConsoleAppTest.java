a7dc36544715311c896a47f637582561eb015eb5
Polina <polina-postylyakova@yandex.ru>
2021-03-25 23:29:57 +0300

    First 3 fixes

--- src/test/java/ls/ConsoleAppTest.java
+++ src/test/java/ls/ConsoleAppTest.java
@@ -3,7 +3,6 @@ package ls;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,60 +10,60 @@ class ConsoleAppTest {
 
     @Test
     void timeOfLastModification() {
-        File test = new File("src//main//resources//java.PNG");
+        File test = new File("src//test//java//resources//java.PNG");
         assertEquals(ConsoleApp.timeOfLastModification(test), "22.03.2021 00:25:56");
-        File test1 = new File("src//main//resources//kotlin.PNG");
+        File test1 = new File("src//test//java//resources//kotlin.PNG");
         assertEquals(ConsoleApp.timeOfLastModification(test1), "22.03.2021 00:25:25");
-        File test2 = new File("src//main//resources//one.txt");
+        File test2 = new File("src//test//java//resources//one.txt");
         assertEquals(ConsoleApp.timeOfLastModification(test2), "22.03.2021 22:06:26");
-        File test3 = new File("src//main//resources//no.txt");
+        File test3 = new File("src//test//java//resources//no.txt");
         assertEquals(ConsoleApp.timeOfLastModification(test3), "01.01.1970 03:00:00");
-        File test4 = new File("src//main//resources//two.txt");
+        File test4 = new File("src//test//java//resources//two.txt");
         assertNotEquals(ConsoleApp.timeOfLastModification(test4), "22.03.2021 00:24:09");
         assertNotEquals(ConsoleApp.timeOfLastModification(test2), "12.12.2012 12:12:12");
         assertNotEquals(ConsoleApp.timeOfLastModification(test), "xyz");
-        File test5 = new File("src//main//resources//5.txt");
+        File test5 = new File("src//test//java//resources//5.txt");
         assertEquals(ConsoleApp.timeOfLastModification(test3), ConsoleApp.timeOfLastModification(test5));
-        File test6 = new File("src//main//resources//folder");
+        File test6 = new File("src//test//java//resources//folder");
         assertEquals(ConsoleApp.timeOfLastModification(test6), "22.03.2021 01:02:39");
 
     }
 
     @Test
     void sizeForHr() {
-        File test = new File("src//main//resources//java.PNG");
+        File test = new File("src//test//java//resources//java.PNG");
         assertEquals(ConsoleApp.sizeForHr(test), "43 Kb");
-        File test1 = new File("src//main//resources//kotlin.PNG");
+        File test1 = new File("src//test//java//resources//kotlin.PNG");
         assertEquals(ConsoleApp.sizeForHr(test1), "22 Kb");
-        File test2 = new File("src//main//resources//three.txt");
+        File test2 = new File("src//test//java//resources//three.txt");
         assertEquals(ConsoleApp.sizeForHr(test2), "0 B");
-        File test3 = new File("src//main//resources//two.txt");
+        File test3 = new File("src//test//java//resources//two.txt");
         assertNotEquals(ConsoleApp.sizeForHr(test3), ConsoleApp.timeOfLastModification(test3));
         assertNotEquals(ConsoleApp.sizeForHr(test3), "2048 B");
         assertEquals(ConsoleApp.sizeForHr(test3), "1 Kb");
-        File test5 = new File("src//main//resources//no.txt");
+        File test5 = new File("src//test//java//resources//no.txt");
         assertNotEquals(ConsoleApp.sizeForHr(test5), "0 Kb");
         assertEquals(ConsoleApp.sizeForHr(test5), "0 B");
     }
 
-    private final File[] result = ConsoleApp.path(new File("src//main//resources"));
+    private final File[] result = ConsoleApp.path(new File("src//test//java//resources"));
 
     @Test
     void path() {
-        File one = new File("src//main//resources//one.txt");
-        File two = new File("src//main//resources//two.txt");
-        File three = new File("src//main//resources//three.txt");
-        File folder = new File("src//main//resources//folder");
-        File java = new File("src//main//resources//java.PNG");
-        File kotlin = new File("src//main//resources//kotlin.PNG");
+        File one = new File("src//test//java//resources//one.txt");
+        File two = new File("src//test//java//resources//two.txt");
+        File three = new File("src//test//java//resources//three.txt");
+        File folder = new File("src//test//java//resources//folder");
+        File java = new File("src//test//java//resources//java.PNG");
+        File kotlin = new File("src//test//java//resources//kotlin.PNG");
         File[] result1 = new File[]{folder, java, kotlin, one, three, two}; // !Нумерация здесь
         assertArrayEquals(result1, result);
-        assertArrayEquals(null, ConsoleApp.path(new File("src//main//error")));
+        assertArrayEquals(null, ConsoleApp.path(new File("src//test//java//error")));
     }
 
     @Test
     void name() {
-        assert result != null;
+        //assert result != null;
         assertEquals("folder", ConsoleApp.getName(result[0]));
         assertEquals("one.txt", ConsoleApp.getName(result[3]));
         assertEquals("java.png", ConsoleApp.getName(result[1]));
